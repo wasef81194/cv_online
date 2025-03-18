32722c57eb1cca1a7764aaf6d1c9409a6acaafd5
Wasef81194 <alex.wasef@gmail.com>
2025-03-18 17:03:01 +0100

    add medias to roject

--- assets/components/projet/Modal.js
+++ assets/components/projet/Modal.js
@@ -55,31 +55,61 @@ const ModalProjet= ({ isOpen, onClose, idProjet }) => {
             ) : (
             <dialog open className='modal modal-projet'>
                 <div className='ctn-modal'>
-                        <div>
-                            <div className='close' onClick={onClose}><i className="bi bi-x-lg"></i></div>
-                            {apiDataProjet && (
-                                <div className='ctn-projet'>
-                                    <div className='infos-projet'>
-                                        <div className='image'>
-                                            <img src={`../images/projets/${apiDataProjet.images[0].name}`} alt="Logo" />
-                                        </div>
-                                        <div className='info-projet'> 
-                                            <div>
-                                                <h2>{apiDataProjet.name}</h2>
-                                                <div className='ecole'>{apiDataProjet.ecole}</div>
-                                            </div>
-                                            <div className='date'>
-                                            <div className='start'>{formatDate(apiDataProjet.dateStart)}</div>
-                                            <div> - </div>
-                                            <div className='end'>{formatDate(apiDataProjet.dateEnd)}</div>
-                                        </div>
+                    <div>
+                        <div className='close' onClick={onClose}><i className="bi bi-x-lg"></i></div>
+                        {apiDataProjet && (
+                            <div className='ctn-projet'>
+                                <div className='infos-projet'>
+                                    <div className='image'>
+                                        <img src={`../images/projets/${apiDataProjet.images[0].name}`} alt="Logo" />
                                     </div>
+                                    <div className='info-projet'> 
+                                        <div>
+                                            <h2>{apiDataProjet.name}</h2>
+                                            <div className='ecole'>{apiDataProjet.ecole}</div>
+                                        </div>
+                                        <div className='date'>
+                                        <div className='start'>{formatDate(apiDataProjet.dateStart)}</div>
+                                        <div> - </div>
+                                        <div className='end'>{formatDate(apiDataProjet.dateEnd)}</div>
                                     </div>
-                                    <div className='description' dangerouslySetInnerHTML={{ __html: apiDataProjet.description }}></div>
                                 </div>
-                            )}
+                                </div>
+                                <div className='description' dangerouslySetInnerHTML={{ __html: apiDataProjet.description }}></div>
+                            </div>
+                        )}
+                    </div>
+                    {apiDataProjet.images?.length > 1 && (
+                        <>
+                        <h3>Aperçu du projet</h3>
+                        <div className='medias-projet'>
+                            {apiDataProjet.images.map((media, index) => {
+                                // Ignorer le premier élément
+                                if (index === 0) return null;
+
+                                // Vérifier si c'est une image ou une vidéo
+                                const isImage = media.name.match(/\.(jpeg|jpg|png|gif)$/i); // Vérifie l'extension du fichier
+
+                                return isImage ? (
+                                    // Si c'est une image
+                                    <a target="_blank"  href={`../images/projets/${media.name}`}title="Voir l'image"className='image-projet'key={index}>
+                                        <img src={`../images/projets/${media.name}`} alt={`Image ${index + 1}`} />
+                                    </a>
+                                ) : (
+                                    // Si c'est une vidéo
+                                    <div className='video-projet' key={index}>
+                                        <video controls>
+                                            <source src={`../images/projets/${media.name}`} type="video/mp4" />
+                                            Votre navigateur ne supporte pas la lecture des vidéos.
+                                        </video>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    
+                        </>
+                    )}
+
+
                 </div>
             </dialog>
             )}
